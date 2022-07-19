e4ebff46ed18e8b2f4c94a902fd580ea16808eb9
Kristopher Eaton <kristophereaton12@gmail.com>
2022-07-18 19:25:15 -0700

    Just test

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.send(`<h2>Hello from ${req.baseUrl}</h2>`);
+  res.send(`<h2>System online at: ${req.baseUrl}</h2>`);
 });
 
 module.exports = router;
